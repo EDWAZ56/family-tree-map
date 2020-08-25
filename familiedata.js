7fad03b7c53eb091f4758de681557cc44fcbf27b
Eduard Dauwe <eduard.dauwe@gmail.com>
2020-08-25 10:14:06 +0200

    startpunt25aug

---
+++ familiedata.js
@@ -0,0 +1,40 @@
+var personen={"type": "FeatureCollection",
+"features":
+  [{"type":"Feature",
+    "properties": {
+        "naam": "Sasha Simonenko",
+		"familie": "Dauwe",
+		"kinderen":[],
+		"geslacht":"v"
+    },
+    "geometry": {
+        "type": "Point",
+  "coordinates": [3,51]}},
+   {"type":"Feature",
+    "properties": {
+        "naam": "Eduard Dauwe",
+		"familie": "Dauwe",
+		"kinderen":["Sasha Simonenko"],
+		"geslacht":"m"
+    },
+    "geometry": {
+        "type": "Point",
+   "coordinates": [2,51]}
+   },
+   {"type":"Feature",
+   "properties":{
+	   "naam":"Thomas Dekeyser",
+	   "familie":"Dekeyser",
+	   "kinderen":[],
+	   "geslacht":"m"	   
+   },
+   "geometry":{
+	   "type":"Point",
+	   "coordinates":[2.5,50.5]
+   }
+   } 
+]
+}
+		
+		
+		
