a396848b6ee992ddc1b9197b63e735eedc747b4b
Eduard Dauwe <eduard.dauwe@gmail.com>
2020-10-01 09:42:58 +0200

    alles

--- googledocurl.js
+++ googledocurl.js
@@ -1,1 +1,2 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1mOBUbBVXFkRgtACWeZU8R3Q0OiOtaKw_SeUhtNbbcF4/edit#gid=164271551';
+var googleDocURLlocal="C:\Users\Eduard Dauwe\Desktop\stamboom\Points.html";
